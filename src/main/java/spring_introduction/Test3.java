edabbbc20c885bd387fecfdaecd7c7c5d5e26800
DA_promos <d.abramovskyi@gmail.com>
2021-07-11 22:04:38 +0200

    Spring Course - Dependency Injection - ID by constructor

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -11,6 +11,7 @@ public class Test3 {
 //        Pet pet = context.getBean("myPet", Pet.class);
 //                Pet pet = new Dog();
 
+
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
         context.close();
