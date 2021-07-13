d42d9fe8b9a829a89f5afc3887d5bfafd644ff44
DA_promos <d.abramovskyi@gmail.com>
2021-07-13 22:13:13 +0200

    Spring Course - Dependency Injection

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -15,7 +15,9 @@ public class Test3 {
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
 
-//
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+
 //        Person workPlace = context.getBean("myWork", Person.class);
 //        workPlace.showYourWork();
 
