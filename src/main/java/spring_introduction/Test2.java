3301b50e4756874103bd7f805cbff01772b70aa2
DA_promos <d.abramovskyi@gmail.com>
2021-07-28 22:26:23 +0200

    Spring Course - Bean Scope

--- src/main/java/spring_introduction/Test2.java
+++ src/main/java/spring_introduction/Test2.java
@@ -3,6 +3,9 @@ package spring_introduction;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test2 {
+    public Test2(Dog dog) {
+    }
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
--- src/main/java/spring_introduction/Test4.java
+++ src/main/java/spring_introduction/Test4.java
@@ -7,5 +7,19 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
 
+        Dog myDog = context.getBean("myPet", Dog.class);
+        myDog.setName("Belka");
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.setName("Strelka");
+
+        System.out.println(myDog.getName());
+        System.out.println(yourDog.getName());
+
+        System.out.println("Variables refer to the same objects = " + (myDog==yourDog));
+        System.out.println(myDog);
+        System.out.println(yourDog);
+
+        context.close();
+
     }
 }
