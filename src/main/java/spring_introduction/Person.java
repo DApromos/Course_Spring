857ded4d483ba762263beb2e2e0941a580b255f0
DA_promos <d.abramovskyi@gmail.com>
2021-08-02 22:47:13 +0200

    Spring Course - Configuration using @Annotations for methods

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,10 +1,13 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
+    @Autowired
+    @Qualifier("myPet")
     private Pet pet;
     private String surname;
     private int age;
