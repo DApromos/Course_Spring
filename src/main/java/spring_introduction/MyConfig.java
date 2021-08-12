7ca3840cfc2ad6662f0b637bc5e21c8b8d1e6a7e
DA_promos <d.abramovskyi@gmail.com>
2021-08-12 22:34:44 +0200

    Spring Course - Spring Container Configuration using Java Code - Second method

--- src/main/java/spring_introduction/MyConfig.java
+++ src/main/java/spring_introduction/MyConfig.java
@@ -1,19 +1,22 @@
 package spring_introduction;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Configuration
-@ComponentScan("spring_introduction")
+@PropertySource("classpath:myApp.properties")
+//@ComponentScan("spring_introduction")
 public class MyConfig {
 
     @Bean
     @Scope("singleton")
     public Pet catBean(){
         return new Cat();
+    }
+
+    @Bean
+    public Person personBean() {
+        return new Person(catBean());
     }
 
 }
